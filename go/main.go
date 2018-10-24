5fdb90275bbd8b1f0c6ed7e89669b3d80de77693
楊野 良智 <yasunoyoshimoto@LAB-N870.local>
2018-10-24 11:41:48 +0900

    Add bernouli tent

--- go/main.go
+++ go/main.go
@@ -6,9 +6,12 @@ import (
 	"math"
 	"os"
 	"strconv"
+	"sync"
 )
 
 var sc = bufio.NewScanner(os.Stdin)
+var wg = sync.WaitGroup{}
+var rw = sync.RWMutex{}
 
 func main() {
 	fmt.Println("初期値を入力してください")
@@ -18,14 +21,40 @@ func main() {
 	fmt.Println("繰り返し回数を入力してください")
 	n := ReadInt()
 	//result := logisticMapBig(big.NewFloat(xo), big.NewFloat(a), n)
-	result := logisticMap(xo, a, n)
-	if math.IsInf(result[n-1], -1) {
+	wg.Add(1)
+	go func() {
+		result := logisticMap(xo, a, n)
+		judgeDemeanor(result[n-1], "ロジスティック写像")
+		wg.Done()
+	}()
+
+	wg.Add(1)
+	go func() {
+		resultTent := tentMap(xo, a, n)
+		judgeDemeanor(resultTent[n-1], "テント写像")
+		wg.Done()
+	}()
+
+	wg.Add(1)
+	go func() {
+		resultBernouli := bernouliMap(xo, a, n)
+		judgeDemeanor(resultBernouli[n-1], "ベルヌーイ写像")
+		wg.Done()
+	}()
+	wg.Wait()
+}
+
+func judgeDemeanor(value float64, funcName string) {
+	rw.Lock()
+	fmt.Println(funcName + "は")
+	if math.IsInf(value, -1) {
 		fmt.Println("マイナス無限大に発散しました。\n")
-	} else if math.IsInf(result[n-1], 1) {
+	} else if math.IsInf(value, 1) {
 		fmt.Println("無限大に発散しました。\n")
 	} else {
-		fmt.Printf("%vに収束しました。\n", result[n-1])
+		fmt.Printf("%vに収束しました。\n", value)
 	}
+	rw.Unlock()
 }
 
 func ReadFloat() float64 {
